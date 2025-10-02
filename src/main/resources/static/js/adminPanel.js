06bbe34cc429adc1c1d3ae0eac48adcdc9c1a49e
Fort163 <Front-91@mail.ru>
2025-10-02 20:40:03 +0400

    resource for admin panel

--- src/main/resources/static/js/adminPanel.js
+++ src/main/resources/static/js/adminPanel.js
@@ -1,12 +1,25 @@
 
 function select(dataset) {
     const form = document.getElementById(dataset.form);
-    const field = document.getElementById(dataset.field)
-    field.value = dataset.value
+    if(dataset.field){
+        const field = document.getElementById(dataset.field)
+        field.value = dataset.value
+    }
     form.submit;
 }
 
 function selectAjax(dataset) {
+    $.ajax({
+        url: dataset.url,
+        type: dataset.method,
+        data: dataset.data,
+        success(response){
+            $('body').html(response);
+        }
+    });
+}
+
+function selectAjaxModel(dataset) {
     $.ajax({
         url: dataset.url,
         type: dataset.method,
